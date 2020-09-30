6779c624e926f6e72301bfc8e0e11b94c9230d34
Arickaji <66716199+Arickaji@users.noreply.github.com>
2020-09-30 19:37:46 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,10 @@
 # Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
+try:
+  num1 = input('Enter first number: ')
+  num2 = input('Enter second number: ')
 
 # this is a very simple You can use print statement 
-print(f"Total answer is {num1+num2}.") 
+  print(f"Total answer is {num1+num2}.") 
+
+  except:
+    print("Please check your input ")
